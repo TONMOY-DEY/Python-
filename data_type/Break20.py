cc7d30d11fa94092db96bffa486569ceaa61c8f0
TONMOY-DEY <tonmoydey2025@gmail.com>
2025-09-07 02:05:25 +0600

    sort file

---
+++ data_type/Break20.py
@@ -0,0 +1,14 @@
+pocket_e_tk=100
+
+while pocket_e_tk>=15:
+    print("Mynebag a ache ",pocket_e_tk)
+    pocket_e_tk=pocket_e_tk-15
+
+    if pocket_e_tk < 0 :
+        break
+
+    print("Icecream kinbo")
+    print("incecream khabo")
+    print("Manyag e ache",pocket_e_tk)
+
+    print("La la ! I am happy")
---
+++ data_type/practice.py
@@ -0,0 +1,43 @@
+# res=0
+
+# a=1
+# while a<=5:
+#  print(a)
+#  a=a+1
+
+# # print(a)
+# # a=a+1
+
+# # print(a)
+# # a=a+1
+
+# # print(a)
+
+
+
+
+# #user input
+# a=1
+# end=int(input())
+
+# while a<=end:
+#  print(a)
+#  a=a+1
+
+res=0
+a=int(input())
+n=int(input())
+
+while a<=n:
+ res=res+a
+ a=a+1
+ print(res)
+# res=res+a
+# a=a+1
+# res=res+a
+# a=a+1
+# res=res+a
+# a=a+1
+# res=res+a
+#print(res)
+
