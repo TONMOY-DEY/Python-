2f0a1c9c457eda84e760bd50614717bff143848e
TONMOY-DEY <tonmoydey2025@gmail.com>
2025-09-06 23:48:55 +0600

    some file add

---
+++ data_type/1_to_100_sum.py
@@ -0,0 +1,14 @@
+res=0
+
+a=1
+while a<=5:
+ print(a)
+ a=a+1
+
+# print(a)
+# a=a+1
+
+# print(a)
+# a=a+1
+
+# print(a)
---
+++ data_type/While.py
@@ -0,0 +1,8 @@
+pocket_e_tk=100
+
+while pocket_e_tk>=15:
+    print("Mynebag a ache ",pocket_e_tk)
+    pocket_e_tk=pocket_e_tk-15
+    print("Icecream kinbo")
+    print("incecream khabo")
+    print("Manyag e ache",pocket_e_tk)
