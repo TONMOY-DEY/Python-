dcfe9a072f896a15c77b187b58f09ecbb87a3968
TONMOY-DEY <tonmoydey2025@gmail.com>
2025-09-09 03:05:52 +0600

    some file add

---
+++ data_type/salary.py
@@ -0,0 +1,12 @@
+emplotyes_numbe=int(input())
+
+per_month_work_time=float(input())
+
+per_month_work_salary=float(input())
+
+print("emplotyes_numbe",emplotyes_numbe)
+
+salary=per_month_work_time*per_month_work_salary
+
+print(f"Number of employee{emplotyes_numbe}")
+print(f"Salary{salary}")
