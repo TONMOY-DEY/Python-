ff8cfe19884afe37de1831d56464b9a6af216b73
TONMOY-DEY <tonmoydey2025@gmail.com>
2025-09-11 23:57:39 +0600

    some file add

---
+++ data_type/Functiob32_call_defenation.py
@@ -0,0 +1,19 @@
+def routine():
+
+    print("Making Breakfast......")
+    print("Making Tea...")
+    print("Making brade...")
+    print("Cofee....")
+
+
+
+
+routine()
+
+
+print("ghome astache")
+print("good night")
+
+routine()
+
+
