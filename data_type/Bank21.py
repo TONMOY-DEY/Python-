0fa489a18bfab652c8f022b2942627b004b4453d
TONMOY-DEY <tonmoydey2025@gmail.com>
2025-09-23 16:51:35 +0600

    some file add

--- data_type/Bank21.py
+++ data_type/Bank21.py
@@ -5,9 +5,10 @@ class BankAccount:
 class savingAccount(BankAccount):
   Account_holder="Mr.X"
 
-class CurrentAccount:
+class CurrentAccount(savingAccount):
     pass 
 
-sa=savingAccount()
+sa=CurrentAccount()
 print(sa.balance)
 print(sa.Account_holder)
+print(sa.account_number)
